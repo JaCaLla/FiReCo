c06a74051e0d3c95aa4cb2f8ada00dfef43657b9
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2025-02-10 15:48:47 +0100

    Base project

---
+++ FiReCo/FiReCoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiReCoApp.swift
+//  FiReCo
+//
+//  Created by Javier Calatrava on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FiReCoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
